74e25ab4ef2eea129171b79ba04d437ba4ab2afb
Lauro Moura <lmoura@igalia.com>
2021-10-06 00:21:39 -0300

    fetch.py: Default output to its own directory

--- fetch.py
+++ fetch.py
@@ -12,19 +12,22 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-d', '--dest-dir', metavar='DEST', help='Destination directory',
-        default=os.getcwd())
+        default=None)
 
     return parser.parse_args()
 
 def main():
 
     args = parse_args()
+    mydir = os.path.dirname(os.path.abspath(__file__))
 
-    if not os.path.isdir(args.dest_dir):
+    if args.dest_dir and not os.path.isdir(args.dest_dir):
         print("Destination must be a valid directory")
         sys.exit(1)
+    elif not args.dest_dir:
+        args.dest_dir = mydir
 
-    with open('bots.json') as handle:
+    with open(os.path.join(mydir, 'bots.json')) as handle:
         data = json.load(handle)
 
 
